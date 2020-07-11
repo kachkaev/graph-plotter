6de8425632b57dfc183a7300c187f9f4dc444dcf
Alexander Kachkaev <alexander@kachkaev.ru>
2020-07-11 21:49:46 +0100

    Slightly amend styles

--- src/ui/PageContentsForApp/App/shared/Input.tsx
+++ src/ui/PageContentsForApp/App/shared/Input.tsx
@@ -9,7 +9,7 @@ const Wrapper = styled.div<{ status?: InputStatus }>`
   height: 24px;
   vertical-align: baseline;
   display: flex;
-  padding: 0 3px;
+  padding: 0 5px;
   border: 1px solid;
   border-color: ${(p) => (p.status === "error" ? "#dd9e8c" : "#cacaca")};
   background: ${(p) =>
@@ -25,6 +25,8 @@ const Prefix = styled.span`
   display: inline-block;
   white-space: pre;
   flex-grow: 0;
+  padding: 0;
+  line-height: 1.7;
 `;
 
 const InputControl = styled.input<{ textAlign: InputTextAlign }>`
@@ -33,6 +35,8 @@ const InputControl = styled.input<{ textAlign: InputTextAlign }>`
   background: transparent;
   border: none;
   flex-grow: 1;
+  padding: 0;
+  line-height: 1.5;
 
   :focus {
     outline: none;
