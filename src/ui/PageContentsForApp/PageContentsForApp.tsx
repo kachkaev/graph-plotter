6e695b18b8f7d7aa6c32fd44449c63c8d9e8dd40
Alexander Kachkaev <alexander@kachkaev.ru>
2020-11-28 14:42:23 +0000

    Reduce iframe size

--- src/ui/PageContentsForApp/PageContentsForApp.tsx
+++ src/ui/PageContentsForApp/PageContentsForApp.tsx
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
 export const PageContentsForApp: React.FunctionComponent = () => {
   return (
     <Wrapper>
-      <App width={800} height={600} />
+      <App width={780} height={570} />
     </Wrapper>
   );
 };
