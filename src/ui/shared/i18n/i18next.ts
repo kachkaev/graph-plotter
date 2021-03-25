736e0b810569fc56761f28b8c41d194b3bc8ec24
Alexander Kachkaev <alexander@kachkaev.ru>
2021-03-25 08:30:20 +0000

    Bump dependencies

--- src/ui/shared/i18n/i18next.ts
+++ src/ui/shared/i18n/i18next.ts
@@ -1,17 +1,9 @@
 import i18next from "i18next";
 import ICU from "i18next-icu";
-import de from "i18next-icu/locale-data/de";
-import en from "i18next-icu/locale-data/en";
-import ru from "i18next-icu/locale-data/ru";
-import uk from "i18next-icu/locale-data/uk";
 
 import { defaultLanguage, localeResourceLookup } from "./localeResources";
 
 const icu = new ICU();
-icu.addLocaleData(de);
-icu.addLocaleData(en);
-icu.addLocaleData(ru);
-icu.addLocaleData(uk);
 i18next.use(icu);
 
 i18next.init({
