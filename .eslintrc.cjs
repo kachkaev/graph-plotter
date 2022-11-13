7ebea499a2025a0ecd9fef63292ab2928f873028
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-13 13:24:19 +0000

    Update linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,7 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 module.exports = {
   extends: [
     "@kachkaev/eslint-config-react",
     "@kachkaev/eslint-config-react/extra-type-checking",
     "plugin:@next/next/recommended",
   ],
-  rules: {
-    "no-restricted-syntax": "off",
-  },
 };
